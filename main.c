84d66a379b5d4b125a4e1842ed06308061cefac6
Emilien <emilien.vitry-lhotte@etu.unilasalle.fr>
2026-01-06 09:45:21 +0100

    remove line

--- main.c
+++ main.c
@@ -170,7 +170,7 @@ int main( int argc, char* args[] ) {
 
     bool quit = false;
     SDL_Event e;
-    initrex();
+
     while( !quit ) {
         while( SDL_PollEvent( &e ) != 0 ) {
             if( e.type == SDL_QUIT ) quit = true;
